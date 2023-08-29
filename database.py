c89bef9f9a6c859a7b6c35a3bcab41a78322b368
Gaston <paez.gastonm@gmail.com>
2023-08-28 23:51:00 -0300

    Fix DDBB

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 import certifi
 
-MONGO_URI = 'mongodb+srv://gpdevsl:<password>@cluster0.zd0ohox.mongodb.net/?retryWrites=true&w=majority'
+MONGO_URI = 'mongodb+srv://slgpdevsl:x8FQrIUdqTzW9QDc@cluster0.l68qbl0.mongodb.net/?retryWrites=true&w=majority'
 ca = certifi.where()
 
 def dbConnection():
     try:
-        client = MongoClient.connect(MONGO_URI, tlsCAFile=ca)
+        client = MongoClient(MONGO_URI,tlsCAFile=ca)
         db = client["ddbb_products_app"]
     except ConnectionError:
         print("Error de conexion con la bbdd")
